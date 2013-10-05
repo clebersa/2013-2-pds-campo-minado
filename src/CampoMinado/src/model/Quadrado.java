191387d37028ac70b54e4a4a2f7d5de4ebc58fa9
cleber.93cd@gmail.com <cleber.93cd@gmail.com@5e9439f2-5b89-d2f7-844b-a047a80f462d>
2013-10-05 03:12:31 +0000

    Adicionando JAVADOC em Quadrado.java

--- src/CampoMinado/src/model/Quadrado.java
+++ src/CampoMinado/src/model/Quadrado.java
@@ -17,31 +17,50 @@ public class Quadrado implements Exibivel {
     }
 
     /**
-     * Abre o quadrado, mudando o valor da variável @see #aberto para true. Se o
-     * quadrado tiver uma mina, o método retorna false. Caso contrário, retorna
-     * true.
+     * Abre o quadrado, mudando o valor de aberto para true.
+     *
+     * @return Tipo do conteúdo do quadrado.
      */
     public TipoConteudo abrir() {
-        return null;
+        return conteudo.getTipo();
     }
 
     /**
-     * Marca o quadrado
+     * Marca o quadrado.
      */
     public void marcar() {
-        abrir();
+        //marcar o quadrado usando a referência de marcação
     }
 
+    /**
+     * Adiciona uma mina a este quadrado, modificando seu conteúdo.
+     */
     public void adicionarMina() {
+        //modificar o conteúdo para mina usando a referência de conteúdo
     }
 
+    /**
+     * Contabiliza o número de minas que estão presentes na vizinhança do
+     * quadrado, incrementando esse número em 1 (uma) unidade.
+     */
     public void contabilizarMinaVizinha() {
+        //incrementar +1 no número de minas vizinhas.
     }
 
+    /**
+     * Exibe um quadrado.
+     */
     public void exibir() {
+        //A exibição deve ser feita com System.out.print, segundo o formato [L],
+        //onde L representa o q será apresentado. No caso, se o quadrado estiver fechado, exibe a marcação
+        // se estiver aberto, exibe o conteúdo.
     }
 
+    /**
+     * Indica se o quadrado contém uma mina ou não.
+     * @return TRUE se o quadrado contém uma mina ou FALSE, caso contrário.
+     */
     public boolean contemMina() {
-        return false;
+        //verificar conteudo para retornar a informação correta
     }
 }
