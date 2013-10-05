202ee0ba7fecb56235b3f693eda7aed6582cfd35
cleber.93cd@gmail.com <cleber.93cd@gmail.com@5e9439f2-5b89-d2f7-844b-a047a80f462d>
2013-10-05 03:40:57 +0000

    Adicionando JAVADOC em Quadrado.java

--- trunk/src/CampoMinado/src/model/TipoTabuleiro.java
+++ trunk/src/CampoMinado/src/model/TipoTabuleiro.java
@@ -2,40 +2,44 @@ package model;
 
 public enum TipoTabuleiro {
 
-	;
+    INICIANTE(new InfoTipoTabuleiro(9, 9, 10)),
+    INTERMEDIARIO(new InfoTipoTabuleiro(16, 16, 40)),
+    AVANCADO(new InfoTipoTabuleiro(20, 24, 99));
+    private final InfoTipoTabuleiro infoTipoTabuleiro;
 
-	private TipoTabuleiro.InfoTipoTabuleiro INICIANTE;
+    TipoTabuleiro(InfoTipoTabuleiro infoTipoTabuleiro) {
+        this.infoTipoTabuleiro = infoTipoTabuleiro;
+    }
 
-	private TipoTabuleiro.InfoTipoTabuleiro INTERMEDIARIO;
+    /**
+     * Obtém o número de linhas do tipo do tabuleiro.
+     */
+    public int getLinhas() {
+        //retornar as linhas
+        return infoTipoTabuleiro.linhas;
+    }
 
-	private TipoTabuleiro.InfoTipoTabuleiro AVANCADO;
+    /**
+     * Obtém o número de colunas do tipo do tabuleiro.
+     */
+    public int getColunas() {
+        //retornar as colunas
+    }
 
-	private TipoTabuleiro.InfoTipoTabuleiro infoTipoTabuleiro;
+    /**
+     * Obtém o número de minas do tipo do tabuleiro.
+     */
+    public int getMinas() {
+        //retornar as minas
+    }
 
-	public void getLinhas() {
+    private static class InfoTipoTabuleiro {
 
-	}
+        private int linhas;
+        private int colunas;
+        private int minas;
 
-	public void getColunas() {
-
-	}
-
-	public void getMinas() {
-
-	}
-
-	private class InfoTipoTabuleiro {
-
-		private int linhas;
-
-		private int colunas;
-
-		private int minas;
-
-		public void InfoTipo(int linhas, int colunas, int minas) {
-
-		}
-
-	}
-
+        public InfoTipoTabuleiro(int linhas, int colunas, int minas) {
+        }
+    }
 }
