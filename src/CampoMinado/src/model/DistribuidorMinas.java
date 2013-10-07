57de5c018c06859383315a22070925224f0bf240
cleber.93cd@gmail.com <cleber.93cd@gmail.com@5e9439f2-5b89-d2f7-844b-a047a80f462d>
2013-10-07 03:20:04 +0000

    Atualizando javadoc. Corrigindo javadoc de Conteudo.java. As classes de distribuição estão prontas para implementação.

--- src/CampoMinado/src/model/DistribuidorMinas.java
+++ src/CampoMinado/src/model/DistribuidorMinas.java
@@ -2,7 +2,9 @@ package model;
 
 public interface DistribuidorMinas extends DistribuidorMinasInterface {
 
-    private Tabuleiro tabuleiro;
-
+	/**
+	 * Distribui as minas sobre um tabuleiro.
+	 * @param tabuleiro Tabuleiro sobre o qual as minas serão distribuídas.
+	 */
     public abstract void distribuirMinas(Tabuleiro tabuleiro);
 }
