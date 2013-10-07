d4248ef7330688f208411cb876ef4113282a1147
cleber.93cd@gmail.com <cleber.93cd@gmail.com@5e9439f2-5b89-d2f7-844b-a047a80f462d>
2013-10-07 03:28:52 +0000

    Inserindo javadoc. Inserindo métodos de definição (set) em Jogada.java.

--- trunk/src/CampoMinado/src/model/Jogada.java
+++ trunk/src/CampoMinado/src/model/Jogada.java
@@ -2,47 +2,74 @@ package model;
 
 public class Jogada {
 
-    private int linha;
-    private int coluna;
-    private TipoJogada tipoJogada;
+	private int linha;
+	private int coluna;
+	private TipoJogada tipoJogada;
 
-    /**
-     * Cria uma nova jogada.
-     *
-     * @param linha Número da linha do quadrado.
-     * @param coluna Número da coluna do quadrado.
-     * @param tipo Tipo da jogada que será realizada sobre o quadrado.
-     */
-    public void Jogada(int linha, int coluna, TipoJogada tipo) {
+	/**
+	 * Cria uma nova jogada.
+	 *
+	 * @param linha Número da linha do quadrado.
+	 * @param coluna Número da coluna do quadrado.
+	 * @param tipo Tipo da jogada que será realizada sobre o quadrado.
+	 */
+	public void Jogada(int linha, int coluna, TipoJogada tipo) {
+		setLinha(linha);
+		setColuna(coluna);
+		setTipo(tipo);
+	}
+
+	/**
+	 * Obtém o número da linha do quadrado.
+	 *
+	 * @return Número da linha do quadrado.
+	 */
+	public int getLinha() {
+		return linha;
+	}
+
+	/**
+	 * Define o número da linha do quadrado.
+	 *
+	 * @param linha Número da linha do quadrado.
+	 */
+	private void setLinha(int linha) {
 		this.linha = linha;
+	}
+
+	/**
+	 * Obtém o número da coluna do quadrado.
+	 *
+	 * @return Número da coluna do quadrado.
+	 */
+	public int getColuna() {
+		return coluna;
+	}
+
+	/**
+	 * Define o número da coluna do quadrado.
+	 *
+	 * @param coluna Número da coluna do quadrado.
+	 */
+	private void setColuna(int coluna) {
 		this.coluna = coluna;
-		this.tipoJogada = tipo;
-    }
+	}
 
-    /**
-     * Obtém o número da linha do quadrado.
-     *
-     * @return Número da linha do quadrado.
-     */
-    public int getLinha() {
-        return linha;
-    }
+	/**
+	 * Obtém o tipo da jogada.
+	 *
+	 * @return Tipo da jogada.
+	 */
+	public TipoJogada getTipo() {
+		return this.tipoJogada;
+	}
 
-    /**
-     * Obtém o número da coluna do quadrado.
-     *
-     * @return Número da coluna do quadrado.
-     */
-    public int getColuna() {
-        return coluna;
-    }
-
-    /**
-     * Obtém o tipo da jogada.
-     *
-     * @return Tipo da jogada.
-     */
-    public TipoJogada getTipo() {
-        return this.tipoJogada;
-    }
+	/**
+	 * Define o tipo da jogada.
+	 *
+	 * @param tipoJogada Tipo da jogada.
+	 */
+	private void setTipo(TipoJogada tipoJogada) {
+		this.tipoJogada = tipoJogada;
+	}
 }
