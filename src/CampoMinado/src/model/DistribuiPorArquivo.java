92ad9f0436bf19f1fc9258647de53e766d27b4ba
vetor39@gmail.com <vetor39@gmail.com@5e9439f2-5b89-d2f7-844b-a047a80f462d>
2013-10-15 02:47:51 +0000

    Métodos exibir (Tabuleiro) e IniciarJogor e main da classe CampoMinado implementados

--- src/CampoMinado/src/model/DistribuiPorArquivo.java
+++ src/CampoMinado/src/model/DistribuiPorArquivo.java
@@ -13,58 +13,53 @@ import java.io.IOException;
  */
 public class DistribuiPorArquivo implements DistribuidorMinas {
 
-	/**
-	 * Indica o caminho, absoluto ou relativo, do arquivo que contém a
-	 * distribuição das minas.
-	 */
-	private String caminhoArquivo = ".";
-	/**
-	 * Indica o nome do arquivo que contém a distribuição das minas.
-	 */
-	private String nomeArquivo = "distribuicaoMinas.txt";
+    /**
+     * Indica o caminho, absoluto ou relativo, do arquivo que contém a
+     * distribuição das minas.
+     */
+    private String caminhoArquivo = ".";
+    /**
+     * Indica o nome do arquivo que contém a distribuição das minas.
+     */
+    private String nomeArquivo = "distribuicaoMinas.txt";
 
-	/**
-	 * Distribui as minas sobre o tabuleiro a partir da leitura de um arquivo. A
-	 * primeira linha do arquivo contém a quantidade
-	 * <code>n</code> de minas que serão distribuídas no tabuleiro. As
-	 * <code>n</code> linhas seguintes contém as posições (linha e coluna) de
-	 * cada uma das
-	 * <code>n</code> minas do tabuleiro.
-	 *
-	 * @see model.DistribuidorMinas#distribuirMinas(model.Tabuleiro)
-	 */
-	@Override
-	public void distribuirMinas(Tabuleiro tabuleiro) {
-		int linha, coluna;
-		String dadosArquivos;
+    /**
+     * Distribui as minas sobre o tabuleiro a partir da leitura de um arquivo. A
+     * primeira linha do arquivo contém a quantidade
+     * <code>n</code> de minas que serão distribuídas no tabuleiro. As
+     * <code>n</code> linhas seguintes contém as posições (linha e coluna) de
+     * cada uma das
+     * <code>n</code> minas do tabuleiro.
+     *
+     * @see model.DistribuidorMinas#distribuirMinas(model.Tabuleiro)
+     */
+    @Override
+    public void distribuirMinas(Tabuleiro tabuleiro) {
+        int linha, coluna;
+        String dadosArquivos;
 
-		try {
-			File file;
-			if (tabuleiro.getTipoTabuleiro() == 0) {
-				file = new File("iniciante");
-			} else if (tabuleiro.getTipoTabuleiro() == 1) {
-				file = new File("intermediario");
-			} else {
-				file = new File("avancado");
-			}
-
-			FileReader fileReader = null;
-			fileReader = new FileReader(file);
-			BufferedReader reader = new BufferedReader(fileReader);
-
-			try {
-				while ((dadosArquivos = reader.readLine()) != null) {
-					linha = Integer.parseInt(dadosArquivos) / 10;
-					coluna = Integer.parseInt(dadosArquivos) % 10;
-					tabuleiro.adicionarMina(linha, coluna);
-				}
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		} catch (FileNotFoundException ex) {
-			ex.printStackTrace();
-		}
-
-		//Implementar distribuição das minas pora arquivo.
-	}
+        try {
+            File file = new File("mapadasminas");
+            FileReader fileReader = null;
+            fileReader = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fileReader);
+            try {
+                while ((dadosArquivos = reader.readLine()) != null) {
+                    linha = Integer.parseInt(dadosArquivos) / 10;
+                    coluna = Integer.parseInt(dadosArquivos) % 10;
+                    
+                    if (linha <= tabuleiro.getLinhas() - 1 && coluna <= tabuleiro.getColunas() - 1) {
+                        tabuleiro.adicionarMina(linha, coluna);
+                    }
+                    
+                    
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        //Implementar distribuição das minas pora arquivo.
+    }
 }
--- src/CampoMinado/src/model/Quadrado.java
+++ src/CampoMinado/src/model/Quadrado.java
@@ -7,90 +7,90 @@ package model;
  */
 public class Quadrado implements Exibivel {
 
-	/**
-	 * Indicador se o quadrado está aberto (
-	 * <code>TRUE</code>) ou vazio (
-	 * <code>FALSE</code>).
-	 */
-	private boolean aberto;
-	private int minasVizinhas;
-	private Conteudo conteudo;
-	private Marcacao marcacao;
+    /**
+     * Indicador se o quadrado está aberto (
+     * <code>TRUE</code>) ou vazio (
+     * <code>FALSE</code>).
+     */
+    private boolean aberto;
+    private int minasVizinhas;
+    private Conteudo conteudo;
+    private Marcacao marcacao;
 
-	/**
-	 * Cria um quadrado. Na criação, o quadrado é criado fechado, com nenhuma
-	 * mina vizinha contabilizada e com conteúdo e marcação padrões.
-	 */
-	Quadrado() {
-		conteudo = new Conteudo();
-		marcacao = new Marcacao();
-		aberto = false;
-		minasVizinhas = 0;
-	}
+    /**
+     * Cria um quadrado. Na criação, o quadrado é criado fechado, com nenhuma
+     * mina vizinha contabilizada e com conteúdo e marcação padrões.
+     */
+    Quadrado() {
+        conteudo = new Conteudo();
+        marcacao = new Marcacao();
+        aberto = false;
+        minasVizinhas = 0;
+    }
 
-	/**
-	 * Abre o quadrado.
-	 *
-	 * @return Tipo do conteúdo do quadrado.
-	 */
-	public TipoConteudo abrir() {
-		aberto = true;
-		return conteudo.getTipo();
-	}
+    /**
+     * Abre o quadrado.
+     *
+     * @return Tipo do conteúdo do quadrado.
+     */
+    public TipoConteudo abrir() {
+        aberto = true;
+        return conteudo.getTipo();
+    }
 
-	/**
-	 * Marca o quadrado.
-	 */
-	public void marcar() {
-		marcacao.marcar();
-	}
+    /**
+     * Marca o quadrado.
+     */
+    public void marcar() {
+        marcacao.marcar();
+    }
 
-	/**
-	 * Adiciona uma mina ao quadrado.
-	 */
-	public void adicionarMina() {
-		conteudo.adicionarMina();
-	}
+    /**
+     * Adiciona uma mina ao quadrado.
+     */
+    public void adicionarMina() {
+            conteudo.adicionarMina();
+    }
 
-	/**
-	 * Contabiliza o número de minas que estão presentes na vizinhança do
-	 * quadrado, incrementando esse número em 1 (uma) unidade.
-	 */
-	public void contabilizarMinaVizinha() {
-		conteudo.setIcone(++minasVizinhas);
-	}
+    /**
+     * Contabiliza o número de minas que estão presentes na vizinhança do
+     * quadrado, incrementando esse número em 1 (uma) unidade.
+     */
+    public void contabilizarMinaVizinha() {
+        conteudo.setIcone(++minasVizinhas);
+    }
 
-	/**
-	 * Indica se o quadrado está aberto ou não.
-	 *
-	 * @return <code>TRUE</code> se o quadrado está aberto
-	 * ou <code>FALSE</code>, caso contrário.
-	 */
-	public boolean isAberto() {
-		return aberto;
-	}
+    /**
+     * Indica se o quadrado está aberto ou não.
+     *
+     * @return <code>TRUE</code> se o quadrado está aberto
+     * ou <code>FALSE</code>, caso contrário.
+     */
+    public boolean isAberto() {
+        return aberto;
+    }
 
-	/**
-	 * Indica se o quadrado contém uma mina ou não.
-	 *
-	 * @return <code>TRUE</code> se o quadrado contém uma mina
-	 * ou <code>FALSE</code>, caso contrário.
-	 */
-	public boolean contemMina() {
-		return conteudo.isMINA();
-	}
+    /**
+     * Indica se o quadrado contém uma mina ou não.
+     *
+     * @return <code>TRUE</code> se o quadrado contém uma mina
+     * ou <code>FALSE</code>, caso contrário.
+     */
+    public boolean contemMina() {
+        return conteudo.isMINA();
+    }
 
-	/**
-	 * Exibe o quadrado.
-	 *
-	 * @see model.Exibivel#exibir()
-	 */
-	@Override
-	public void exibir() {
-		if (isAberto()) {
-			System.out.print("[" + conteudo.getIcone() + "]");
-		} else {
-			System.out.print("[" + marcacao.getIcone() + "]");
-		}
-	}
+    /**
+     * Exibe o quadrado.
+     *
+     * @see model.Exibivel#exibir()
+     */
+    @Override
+    public void exibir() {
+        if (isAberto()) {
+            System.out.print("[" + conteudo.getIcone() + "]");
+        } else {
+            System.out.print("[" + marcacao.getIcone() + "]");
+        }
+    }
 }
