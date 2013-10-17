69c4f5b16c6979246b64c400f0656f00e5f8760a
vetor39@gmail.com <vetor39@gmail.com@5e9439f2-5b89-d2f7-844b-a047a80f462d>
2013-10-17 17:16:21 +0000

    Iteradores Implementados parcialmente

--- trunk/src/CampoMinado/src/model/BandeirasIterator.java
+++ trunk/src/CampoMinado/src/model/BandeirasIterator.java
@@ -4,24 +4,38 @@ import java.util.Iterator;
 
 public class BandeirasIterator implements Iterator<Quadrado> {
 
-	private Quadrado quadrados[][];
-	private int linha;
-	private int coluna;
+    private Quadrado quadrados[][];
+    private int linha;
+    private int coluna;
 
-	public void BandeirasIterator(Quadrado quadrados[][]) {
-	}
+    BandeirasIterator(Quadrado quadrados[][]) {
+        this.quadrados = quadrados;
+        linha = 0;
+        coluna = 0;
+    }
 
-	@Override
-	public boolean hasNext() {
-		return false;
-	}
+    @Override
+    public boolean hasNext() {
+        
+        for (int cont_1 = linha; cont_1 < quadrados.length; cont_1++) {
+            for (int cont_2 = coluna; cont_2 < quadrados.length; cont_2++) {
+             //   if (quadrados[cont_1][cont_2].) {
+                    linha = cont_1;
+                    coluna = cont_2;
+                    return true;
+              //  }
+            }
+        }
+        return false;
+        
+    }
 
-	@Override
-	public Quadrado next() {
-		return null;
-	}
+    @Override
+    public Quadrado next() {
+        return null;
+    }
 
-	@Override
-	public void remove() {
-	}
+    @Override
+    public void remove() {
+    }
 }
