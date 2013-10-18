bc6326c8d3fd92f0904de9fa33014b095c825296
cleber.93cd@gmail.com <cleber.93cd@gmail.com@5e9439f2-5b89-d2f7-844b-a047a80f462d>
2013-10-18 01:21:48 +0000

    - Resolvendo conflitos em Tabuleiro.java - Corrigindo métodos com alguma irregularidade. - Adicionando métodos para obter linha e coluna de um quadrado do tabuleiro em TabuleiroIterator.java.

--- src/CampoMinado/src/model/Tabuleiro.java
+++ src/CampoMinado/src/model/Tabuleiro.java
@@ -7,278 +7,272 @@ package model;
  */
 public class Tabuleiro implements Exibivel {
 
-    /**
-     * Tipo do tabuleiro.
-     *
-     * @see model.TipoTabuleiro
-     */
-    private TipoTabuleiro tipoTabuleiro;
-    /**
-     * Matriz de quadrados que formam o tabuleiro.
-     */
-    private Quadrado[][] quadrados;
-    /**
-     * Distribuidor de minas.
-     */
-    private DistribuidorMinas distribuidorMinasInterface;
+	/**
+	 * Tipo do tabuleiro.
+	 *
+	 * @see model.TipoTabuleiro
+	 */
+	private TipoTabuleiro tipoTabuleiro;
+	/**
+	 * Matriz de quadrados que formam o tabuleiro.
+	 */
+	private Quadrado[][] quadrados;
+	/**
+	 * Distribuidor de minas.
+	 */
+	private DistribuidorMinas distribuidorMinasInterface;
 
-    /**
-     * Constrói um novo tabuleiro com base no tipo recebido.
-     *
-     * @param tipo Tipo do tabuleiro.
-     */
-    Tabuleiro(TipoTabuleiro tipo) {
-        /*
-         * Após identificar qual é o tipo, inicializa o tabuleiro, 
-         * distribui as minas e, por fim, regula o tabuleiro com os números 
-         * indicadores de minas na vizinhança.
-         */
-        this.distribuidorMinasInterface = new DistribuiAleatorio(); // Somente p/ testes
-        this.tipoTabuleiro = tipo;
-        this.inicializarTabuleiro();
-        this.distribuirMinas();
-    }
+	/**
+	 * Constrói um novo tabuleiro com base no tipo recebido.
+	 *
+	 * @param tipo Tipo do tabuleiro.
+	 */
+	Tabuleiro(TipoTabuleiro tipo) {
+		distribuidorMinasInterface = new DistribuiAleatorio();
+		tipoTabuleiro = tipo;
+		inicializarTabuleiro();
+		distribuirMinas();
+		contabilizarMinasVizinhas();
+	}
 
-    /**
-     * Executa uma jogada no tabuleiro.
-     *
-     * @param jogada Jogada a ser executada no tabuleiro.
-     * @return <code>FALSE</code> caso a jogada seja 
-     * <code>TipoJogada.ABRIR</code> e o quadrado continha uma mina, ou 
-     * <code>TRUE</code> para jogadas do tipo <code>TipoJogada.MARCAR</code>
-     * e/ou com o quadrado não continha mina.
-     */
-    public boolean executarJogada(Jogada jogada) {
-        boolean resultado;
-        if (jogada.isABRIR()) {
-            resultado = abrirQuadrado(jogada.getLinha(), jogada.getColuna());
-        } else if (jogada.isMARCAR()) {
-            marcarQuadrado(jogada.getLinha(), jogada.getColuna());
-            resultado = true;
-        } else {
-            resultado = false;
-        }
-        return resultado;
-    }
+	/**
+	 * Executa uma jogada no tabuleiro.
+	 *
+	 * @param jogada Jogada a ser executada no tabuleiro.
+	 * @return <code>FALSE</code> caso a jogada seja
+	 * <code>TipoJogada.ABRIR</code> e o quadrado continha uma mina, ou
+	 * <code>TRUE</code> para jogadas do tipo <code>TipoJogada.MARCAR</code>
+	 * e/ou com o quadrado não continha mina.
+	 */
+	public boolean executarJogada(Jogada jogada) {
+		boolean resultado;
+		if (jogada.isABRIR()) {
+			resultado = abrirQuadrado(jogada.getLinha(), jogada.getColuna());
+		} else if (jogada.isMARCAR()) {
+			marcarQuadrado(jogada.getLinha(), jogada.getColuna());
+			resultado = true;
+		} else {
+			resultado = false;
+		}
+		return resultado;
+	}
 
-    /**
-     * Obtém o número de linhas do tabuleiro.
-     *
-     * @return Número de linhas do tabuleiro.
-     */
-    public int getLinhas() {
-        return this.tipoTabuleiro.getLinhas();
-    }
+	/**
+	 * Obtém o número de linhas do tabuleiro.
+	 *
+	 * @return Número de linhas do tabuleiro.
+	 */
+	public int getLinhas() {
+		return this.tipoTabuleiro.getLinhas();
+	}
 
-    /**
-     * Obtém a número de colunas do tabuleiro.
-     *
-     * @return Número de colunas do tabuleiro.
-     */
-    public int getColunas() {
-        return this.tipoTabuleiro.getColunas();
-    }
+	/**
+	 * Obtém a número de colunas do tabuleiro.
+	 *
+	 * @return Número de colunas do tabuleiro.
+	 */
+	public int getColunas() {
+		return this.tipoTabuleiro.getColunas();
+	}
 
-    /**
-     * Obtém a número de minas do tabuleiro.
-     *
-     * @return Número de minas do tabuleiro.
-     */
-    public int getMinas() {
-        return tipoTabuleiro.getMinas();
-    }
+	/**
+	 * Obtém a número de minas do tabuleiro.
+	 *
+	 * @return Número de minas do tabuleiro.
+	 */
+	public int getMinas() {
+		return tipoTabuleiro.getMinas();
+	}
 
-    /**
-     * Adiciona uma mina a um determinado quadrado especificado por
-     * <code>linha</code> e <code>coluna</code>.
-     * @param linha Linha do quadrado.
-     * @param coluna Coluna do quadrado.
-     * @return <code>TRUE</code> caso a mina seja adicionada com sucesso ou
-     * <code>FALSE</code> caso o quadrado já contenha uma mina.
-     */
-    public boolean adicionarMina(int linha, int coluna) {
-        if (!quadrados[linha][coluna].contemMina()) {
-            quadrados[linha][coluna].adicionarMina();
-            return true;
-        }
-        return false;
-    }
+	/**
+	 * Adiciona uma mina a um determinado quadrado especificado por
+	 * <code>linha</code> e
+	 * <code>coluna</code>.
+	 *
+	 * @param linha Linha do quadrado.
+	 * @param coluna Coluna do quadrado.
+	 * @return <code>TRUE</code> caso a mina seja adicionada com sucesso ou
+	 * <code>FALSE</code> caso o quadrado já contenha uma mina.
+	 */
+	public boolean adicionarMina(int linha, int coluna) {
+		Quadrado quadrado;
+		quadrado = getQuadrado(linha, coluna);
+		if (!quadrado.contemMina()) {
+			quadrado.adicionarMina();
+			return true;
+		}
+		return false;
+	}
 
-    /**
-     * Inicializa o tabuleiro com quadrados vazios.
-     */
-    private void inicializarTabuleiro() {
-        this.quadrados = new Quadrado[this.getLinhas()][this.getColunas()];
-        for (int linha = 0; linha < this.getLinhas(); linha++) {
-            for (int coluna = 0; coluna < this.getColunas(); coluna++) {
-                this.quadrados[linha][coluna] = new Quadrado();
-            }
-        }
-    }
+	/**
+	 * Obém um quadrado com base na linha e coluna informadas.
+	 *
+	 * @param linha Linha do quadrado.
+	 * @param coluna Coluna do quadrado.
+	 * @return O quadrado correspondente à linha e coluna informadas.
+	 */
+	private Quadrado getQuadrado(int linha, int coluna) throws ArrayIndexOutOfBoundsException {
+		return quadrados[linha][coluna];
+	}
 
-    /**
-     * Distribui as minas pelo tabuleiro.
-     */
-    private void distribuirMinas() {
-        this.distribuidorMinasInterface.distribuirMinas(this);
-    }
+	/**
+	 * Inicializa o tabuleiro com quadrados vazios.
+	 */
+	private void inicializarTabuleiro() {
+		quadrados = new Quadrado[getLinhas()][getColunas()];
 
-    /**
-     * Para todos os quadrados do tabuleiro, contabiliza o número de minas que
-     * há na vizinhança.
-     */
-    private void contabilizarMinasVizinhas() {
-        /*for (int linha = 0; linha < getLinhas(); linha++) {
-            for (int coluna = 0; coluna < getLinhas(); coluna++) {
-                contabilizarMinasVizinhasQuadrado(linha, coluna);
-            }
-        }*/
-        
-        VizinhosIterator interadorVizinhos;
+		TabuleiroIterator tabuleiroIterator = createTabuleiroIterator();
 
-        for (int cont = 0; cont < quadrados.length; cont++) {
-            for (int cont2 = 0; cont2 < quadrados.length; cont2++) {
-                
-                interadorVizinhos = new VizinhosIterator(quadrados, cont, cont2);
+		Quadrado quadrado;
 
-                while (interadorVizinhos.hasNext()) {
-                    Quadrado tempQuadrado = interadorVizinhos.next();
-                    if (tempQuadrado.contemMina()) {
-                        quadrados[cont][cont2].contabilizarMinaVizinha();
-                    }
-                }
-                
-            }
+		while (tabuleiroIterator.hasNext()) {
+			quadrado = tabuleiroIterator.next();
+			quadrado = new Quadrado();
+		}
+	}
 
-        }
-    }
+	/**
+	 * Distribui as minas pelo tabuleiro.
+	 */
+	private void distribuirMinas() {
+		distribuidorMinasInterface.distribuirMinas(this);
+	}
 
-    /**
-     * Para um quadrado específico, contabiliza a quantidade de minas que há na
-     * vizinhança.
-     *
-     * @param linha Número da linha onde o quadrado está localizado no
-     * tabuleiro.
-     * @param coluna Número da coluna onde o quadrado está localizado no
-     * tabuleiro.
-     */
-    /*private void contabilizarMinasVizinhasQuadrado(int linha, int coluna) {
-        VizinhosIterator interadorVizinhos;
+	/**
+	 * Para todos os quadrados do tabuleiro, contabiliza o número de minas que
+	 * há na vizinhança.
+	 */
+	private void contabilizarMinasVizinhas() {
+		TabuleiroIterator tabuleiroIterator = createTabuleiroIterator();
+		while (tabuleiroIterator.hasNext()) {
+			contabilizarMinasVizinhas(tabuleiroIterator.getLinhaVizinho(),
+					tabuleiroIterator.getColunaVizinho());
+		}
+	}
 
-        for (int cont = 0; cont < quadrados.length; cont++) {
-            for (int cont2 = 0; cont2 < quadrados.length; cont2++) {
-                
-                interadorVizinhos = new VizinhosIterator(quadrados, cont, cont2);
+	/**
+	 * Para um quadrado específico, contabiliza a quantidade de minas que há na
+	 * vizinhança.
+	 *
+	 * @param linha Linha do quadrado.
+	 * @param coluna Coluna do quadrado.
+	 */
+	private void contabilizarMinasVizinhas(int linha, int coluna) {
+		VizinhosIterator interadorVizinhos;
+		interadorVizinhos = new VizinhosIterator(quadrados, linha, coluna);
+		Quadrado vizinho;
+		while (interadorVizinhos.hasNext()) {
+			vizinho = interadorVizinhos.next();
+			if (vizinho.contemMina()) {
+				getQuadrado(linha, coluna).contabilizarMinaVizinha();
+			}
+		}
+	}
 
-                while (interadorVizinhos.hasNext()) {
-                    Quadrado tempQuadrado = interadorVizinhos.next();
-                    if (tempQuadrado.contemMina()) {
-                        quadrados[cont][cont2].contabilizarMinaVizinha();
-                    }
-                }
-            }
+	/**
+	 * Abre um quadrado.
+	 *
+	 * @param linha Número da linha onde o quadrado está localizado no
+	 * tabuleiro.
+	 * @param coluna Número da coluna onde o quadrado está localizado no
+	 * tabuleiro.
+	 * @return <code>FALSE</code>, caso o quadrado contenha uma mina; ou
+	 * <code>TRUE</code>, caso contrário.
+	 */
+	private boolean abrirQuadrado(int linha, int coluna) {
+		Quadrado quadrado;
+		boolean resultado = false;
 
-        }
-        //contabilizar os vizinhos
-    }*/
+		quadrado = getQuadrado(linha, coluna);
+		if (quadrado.isAberto()) {
+			resultado = true;
+		}
+		if (!resultado) {
+			TipoConteudo tipoConteudo;
+			tipoConteudo = quadrado.abrir();
+			if (tipoConteudo.isMINA()) {
+				resultado = false;
+			} else if (tipoConteudo.isNUMERO()) {
+				resultado = true;
+			} else {
+				VizinhosIterator vizinhosIterator = createVizinhosIterator(linha,
+						coluna);
+				while (vizinhosIterator.hasNext()) {
+					abrirQuadrado(vizinhosIterator.getLinhaVizinho(),
+							vizinhosIterator.getColunaVizinho());
+				}
+				resultado = true;
+			}
+		}
 
-    /**
-     * Abre um quadrado.
-     *
-     * @param linha Número da linha onde o quadrado está localizado no
-     * tabuleiro.
-     * @param coluna Número da coluna onde o quadrado está localizado no
-     * tabuleiro.
-     * @return <code>FALSE</code>, caso o quadrado contenha uma mina; ou
-     * <code>TRUE</code>, caso contrário.
-     */
-    private boolean abrirQuadrado(int linha, int coluna) {
-        boolean resultado = false;
-        try {
-            if (quadrados[linha][coluna].isAberto()) {
-                resultado = true;
-            }
-        } catch (IndexOutOfBoundsException excecao) {
-            resultado = true;
-        }
-        if (!resultado) {
-            TipoConteudo tipoConteudo;
-            tipoConteudo = quadrados[linha][coluna].abrir();
-            if (tipoConteudo.isMINA()) {
-                resultado = false;
-            } else if (tipoConteudo.isNUMERO()) {
-                resultado = true;
-            } else {
-                linha -= 1;
-                coluna -= 1;
-                for (int counter = 0; counter < 3; counter++, coluna++) {
-                    abrirQuadrado(linha, coluna);
-                }
-                linha++;
-                coluna--;
-                for (int counter = 0; counter < 2; counter++, linha++) {
-                    abrirQuadrado(linha, coluna);
-                }
-                linha--;
-                coluna--;
-                for (int counter = 0; counter < 2; counter++, coluna--) {
-                    abrirQuadrado(linha, coluna);
-                }
-                linha--;
-                coluna++;
-                for (int counter = 0; counter < 1; counter++, linha--) {
-                    abrirQuadrado(linha, coluna);
-                }
-                resultado = true;
-            }
-        }
+		return resultado;
+	}
 
-        return resultado;
-    }
+	/**
+	 * Marca um quadrado.
+	 *
+	 * @param linha Número da linha onde o quadrado está localizado no
+	 * tabuleiro.
+	 * @param coluna Número da coluna onde o quadrado está localizado no
+	 * tabuleiro.
+	 */
+	private void marcarQuadrado(int linha, int coluna) {
+		Quadrado quadrado = getQuadrado(linha, coluna);
+		quadrado.marcar();
+	}
 
-    /**
-     * Marca um quadrado.
-     *
-     * @param linha Número da linha onde o quadrado está localizado no
-     * tabuleiro.
-     * @param coluna Número da coluna onde o quadrado está localizado no
-     * tabuleiro.
-     */
-    private void marcarQuadrado(int linha, int coluna) {
-        quadrados[linha][coluna].marcar();
-    }
+	/**
+	 * Cria um iterador para os quadrados marcados com bandeira.
+	 *
+	 * @return Um iterador para os quadrados marcados com bandeira.
+	 */
+	public BandeirasIterator createBandeirasIterator() {
+		return new BandeirasIterator(quadrados);
+	}
 
-    public BandeirasIterator createBandeirasIterator() {
-        return new BandeirasIterator(quadrados);
-    }
+	/**
+	 * Cria um iterador para os quadrados vizinhos de um quadrado.
+	 *
+	 * @param linhaQuadrado Linha do quadrado sobre o qual vai se iterar sobre
+	 * os vizinhos.
+	 * @param colunaQuadrado Coluna do quadrado sobre o qual vai se iterar sobre
+	 * os vizinhos.
+	 * @return Um iterador para os quadrados vizinhos de <code>quadrado</code>.
+	 */
+	public VizinhosIterator createVizinhosIterator(int linha, int coluna) {
+		return new VizinhosIterator(quadrados, linha, coluna);
+	}
 
-    public VizinhosIterator createVizinhosIterator(int linhaQuadrado,
-            int colunaQuadrado) {
-        return new VizinhosIterator(quadrados, linhaQuadrado, colunaQuadrado);
-    }
+	/**
+	 * Cria um iterador para os quadrados que contêm minas.
+	 *
+	 * @return Um iterador para os quadrados que contêm minas.
+	 */
+	public MinasIterator createMinasIterator() {
+		return new MinasIterator(quadrados);
+	}
 
-    public MinasIterator createMinasIterator() {
-        return new MinasIterator(quadrados);
-    }
+	/**
+	 * Cria um iterador para todos os quadrados do tabuleiro.
+	 *
+	 * @return Um iterador para todos os quadrados do tabuleiro.
+	 */
+	public TabuleiroIterator createTabuleiroIterator() {
+		return new TabuleiroIterator(quadrados);
+	}
 
-    public TabuleiroIterator createTabuleiroIterator() {
-        return new TabuleiroIterator(quadrados);
-    }
-
-    /**
-     * Exibe o tabuleiro.
-     *
-     * @see model.Exibivel#exibir()
-     */
-    @Override
-    public void exibir() {
-        for (int cont = 0; cont < this.getLinhas(); cont++) {
-            for (int cont2 = 0; cont2 < this.getColunas(); cont2++) {
-                this.quadrados[cont][cont2].exibir();
-            }
-            System.out.println("");
-        }
-        //exibe quadrado por quadrado, até terminar a linha e depois quebra ela.
-        //executa isso até terminar de imprimir toda a matriz.
-    }
+	/**
+	 * Exibe o tabuleiro.
+	 *
+	 * @see model.Exibivel#exibir()
+	 */
+	@Override
+	public void exibir() {
+		for (int cont = 0; cont < this.getLinhas(); cont++) {
+			for (int cont2 = 0; cont2 < this.getColunas(); cont2++) {
+				this.quadrados[cont][cont2].exibir();
+			}
+			System.out.println("");
+		}
+	}
 }
