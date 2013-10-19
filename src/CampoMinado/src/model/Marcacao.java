a0a65b0f7319fa2b8ac7c0fc6742436d89212086
cleber.93cd@gmail.com <cleber.93cd@gmail.com@5e9439f2-5b89-d2f7-844b-a047a80f462d>
2013-10-19 19:56:52 +0000

    - Finalizando javadoc de Marcação.java

--- src/CampoMinado/src/model/Marcacao.java
+++ src/CampoMinado/src/model/Marcacao.java
@@ -1,7 +1,12 @@
 package model;
 
 /**
- * Define os atributos e métodos para uma marcação de um quadrado.
+ * Define os atributos e métodos para a marcação de um quadrado.<br/>Uma
+ * marcação contém um tipo e um ícone, que é o que será exibido por um quadrado,
+ * quando este estiver fechado.<br/>Para cada tipo de marcação, existe um ícone
+ * específico.
+ *
+ * @see TipoMarcacao
  *
  * @author Cleber, Samuel
  */
@@ -11,13 +16,16 @@ public class Marcacao {
 	 * Representação de uma marcação. Os possíveis valores que esta variável
 	 * pode assumir são:<br/>
 	 * '
-	 * <code>&nbsp;</code>' - Ícone que representa nenhuma marcação.<br/>
-	 * '
 	 * <code>B</code>' - Ícone que representa a marcação com bandeira, indicando
-	 * que naquele quadrado há uma mina.<br/>
+	 * que naquele quadrado há uma mina. Este ícone está associado ao tipo
+	 * {@link model.TipoMarcacao#BANDEIRA}.<br/>
 	 * '
 	 * <code>?</code>' - Ícone que representa a marcação com dúvida, quando não
-	 * se tem certeza sobre o que existe dentro do quadrado.
+	 * se tem certeza se existe uma mina no quadrado.Este ícone está associado
+	 * ao tipo {@link model.TipoMarcacao#DUVIDA<br/>
+	 * '
+	 * <code>&nbsp;</code>' - Ícone que representa nenhuma marcação. Este ícone
+	 * está associado ao tipo {@link model.TipoMarcacao#NENHUMA}.<br/>
 	 */
 	private String icone;
 	/**
@@ -38,7 +46,18 @@ public class Marcacao {
 	}
 
 	/**
-	 * Obtém o ícone da marcação.
+	 * Obtém o ícone da marcação.<br/>Este ícone pode ser:<br/>
+	 * '
+	 * <code>B</code>' - Ícone que representa a marcação com bandeira, indicando
+	 * que naquele quadrado há uma mina. Este ícone está associado ao tipo
+	 * {@link model.TipoMarcacao#BANDEIRA}.<br/>
+	 * '
+	 * <code>?</code>' - Ícone que representa a marcação com dúvida, quando não
+	 * se tem certeza se existe uma mina no quadrado.Este ícone está associado
+	 * ao tipo {@link model.TipoMarcacao#DUVIDA<br/>
+	 * '
+	 * <code>&nbsp;</code>' - Ícone que representa nenhuma marcação. Este ícone
+	 * está associado ao tipo {@link model.TipoMarcacao#NENHUMA}.<br/>
 	 *
 	 * @return Ícone da marcação.
 	 */
@@ -48,18 +67,20 @@ public class Marcacao {
 
 	/**
 	 * Obtém o tipo da marcação.
+	 *
 	 * @return Tipo da marcação.
 	 * @see model.TipoMarcacao
 	 */
-	public TipoMarcacao getTipo(){
+	public TipoMarcacao getTipo() {
 		return tipoMarcacao;
 	}
-	
+
 	/**
-	 * Define o ícone e o tipo da marcação. Quando se marca um quadrado pela
-	 * primeira vez, ele é marcado com uma Bandeira. Quando marcado novamente,
-	 * ele é marcado com interrogação. Se marcado novamente, ele é desmarcado,
-	 * assumindo uma marcação vazia.
+	 * Muda a marcação, definindo o ícone e o tipo da marcação.<br/>Quando se
+	 * marca um quadrado pela primeira vez, indica-se que nele há uma
+	 * mina.<br/>Quando marcado novamente, indica-se que não se tem certeza de
+	 * que nele há uma mina.<br/>Se marcado novamente, o quadrado perde a
+	 * marcação e fica dermarcado, ou seja, com nenhuma marcação.
 	 */
 	public void marcar() {
 		if (tipoMarcacao.isNENHUMA()) {
@@ -73,16 +94,17 @@ public class Marcacao {
 			tipoMarcacao = TipoMarcacao.NENHUMA;
 		}
 	}
-	
+
 	/**
-	 * Limpa a marcação, fazendo com que ela tenha o ícone '<code>&nbsp;</code>'
-	 * e tipo <code>TipoMarcacao.NENHUMA</code>.
+	 * Limpa a marcação, fazendo com que ela tenha o ícone '
+	 * <code>&nbsp;</code>' e tipo
+	 * <code>TipoMarcacao.NENHUMA</code>.
 	 */
-	public void limpar(){
-		if(tipoMarcacao.isBANDEIRA()){
+	public void limpar() {
+		if (tipoMarcacao.isBANDEIRA()) {
 			marcar();
 			marcar();
-		}else if(tipoMarcacao.isDUVIDA()){
+		} else if (tipoMarcacao.isDUVIDA()) {
 			marcar();
 		}
 	}
